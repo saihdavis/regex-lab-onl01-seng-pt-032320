c31e6007e4b25c1c7d95d269f699d348701b2273
Saida Davis <saihdavis@gmail.com>
2020-04-10 21:24:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,10 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\A+aeiou/)
+    return true
 end
-
+end
 def words_starting_with_un_and_ending_with_ing(text)
-
+ 
 end
 
 def words_five_letters_long(text)
